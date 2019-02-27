4a5e217162a2b4fc480ffb7cbd3cf992168e393a
afaller <afaller@MSYDLAP0786.int.cggveritas.com>
2019-02-27 16:46:27 +0100

    Create a new local Database for try new component

---
+++ src/database.js
@@ -0,0 +1,39 @@
+export const countryOptions = [
+  { value: 'Inde', label: 'Inde' },
+  { value: 'France', label: 'France' },
+  { value: 'Angleterre', label: 'Angleterre' },
+  { value: 'Espagne', label: 'Espagne' },
+  { value: 'Italie', label: 'Italie' },
+]
+
+export const serviceOptions = [
+  { value: 'PortCall', label: 'PortCall' },
+  { value: 'Helicopter', label: 'Helicopter' }
+]
+
+export const estimate = [
+  { port: 'Lerwick', country: 'UK', currency: 'Dollar' },
+  { port: 'Aberdeen', country: 'UK', currency: 'Dollar' },
+  { port: 'Nouakchott', country: 'Mauritania', currency: 'Dollar' },
+  { port: 'Dakar', country: 'Senegal', currency: 'Dollar' },
+  { port: 'Walvis Bay', country: 'Namibia', currency: 'Dollar' },
+  { port: 'Soyo', country: 'Angola', currency: 'Dollar' },
+  { port: 'Luanda', country: 'Angola', currency: 'Dollar' },
+  { port: 'Beira', country: 'Mozambique', currency: 'Dollar' },
+  { port: 'Tampico', country: 'Mexico', currency: 'Dollar' },
+  { port: 'Vitoria', country: 'Brazil', currency: 'Dollar' },
+  { port: 'Rio', country: 'Brazil', currency: 'Dollar' },
+  { port: 'Galvestone', country: 'USA', currency: 'Dollar' },
+  { port: 'Fourchon', country: 'USA', currency: 'Dollar' },
+  { port: 'Kuching', country: 'Malaysia', currency: 'Dollar' },
+  { port: 'Bintulu', country: 'Malaysia', currency: 'Dollar' },
+  { port: 'Dampier', country: 'Australia', currency: 'Dollar' },
+  { port: 'Melbourne', country: 'Australia', currency: 'Dollar' },
+  { port: 'Stavanger', country: 'Norway', currency: 'Dollar' },
+  { port: 'Bergen', country: 'Norway', currency: 'Dollar' },
+  { port: 'Dunkerque', country: 'France', currency: 'Dollar' },
+  { port: 'Amsterdam', country: 'Netherlands', currency: 'Dollar' },
+  { port: 'Ijmuiden', country: 'Netherlands', currency: 'Dollar' },
+  { port: 'Den Helder', country: 'Netherlands', currency: 'Dollar' },
+  { port: 'Singapore', country: 'Singapore', currency: 'Dollar' }
+]
