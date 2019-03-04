f05be3613806dc06b1e0e54247939e25e4083b67
afaller <afaller@MSYDLAP0786.int.cggveritas.com>
2019-03-04 17:31:22 +0100

    new item in database

--- src/database.js
+++ src/database.js
@@ -12,28 +12,43 @@ export const serviceOptions = [
 ]
 
 export const estimate = [
-  { id:1, port: 'Lerwick', country: 'UK', currency: 'Dollar' },
-  { id:2, port: 'Aberdeen', country: 'UK', currency: 'Dollar' },
-  { id:3, port: 'Nouakchott', country: 'Mauritania', currency: 'Dollar' },
-  { id:4, port: 'Dakar', country: 'Senegal', currency: 'Dollar' },
-  { id:5, port: 'Walvis Bay', country: 'Namibia', currency: 'Dollar' },
-  { id:6, port: 'Soyo', country: 'Angola', currency: 'Dollar' },
-  { id:7, port: 'Luanda', country: 'Angola', currency: 'Dollar' },
-  { id:8, port: 'Beira', country: 'Mozambique', currency: 'Dollar' },
-  { id:9, port: 'Tampico', country: 'Mexico', currency: 'Dollar' },
-  { id:10, port: 'Vitoria', country: 'Brazil', currency: 'Dollar' },
-  { id:11, port: 'Rio', country: 'Brazil', currency: 'Dollar' },
-  { id:12, port: 'Galvestone', country: 'USA', currency: 'Dollar' },
-  { id:13, port: 'Fourchon', country: 'USA', currency: 'Dollar' },
-  { id:14, port: 'Kuching', country: 'Malaysia', currency: 'Dollar' },
-  { id:15, port: 'Bintulu', country: 'Malaysia', currency: 'Dollar' },
-  { id:16, port: 'Dampier', country: 'Australia', currency: 'Dollar' },
-  { id:17, port: 'Melbourne', country: 'Australia', currency: 'Dollar' },
-  { id:18, port: 'Stavanger', country: 'Norway', currency: 'Dollar' },
-  { id:19, port: 'Bergen', country: 'Norway', currency: 'Dollar' },
-  { id:20, port: 'Dunkerque', country: 'France', currency: 'Dollar' },
-  { id:21, port: 'Amsterdam', country: 'Netherlands', currency: 'Dollar' },
-  { id:22, port: 'Ijmuiden', country: 'Netherlands', currency: 'Dollar' },
-  { id:23, port: 'Den Helder', country: 'Netherlands', currency: 'Dollar' },
-  { id:24, port: 'Singapore', country: 'Singapore', currency: 'Dollar' }
+  { id: 1, port: 'Lerwick', country: 'UK', currency: 'Dollar' },
+  { id: 2, port: 'Aberdeen', country: 'UK', currency: 'Dollar' },
+  { id: 3, port: 'Nouakchott', country: 'Mauritania', currency: 'Dollar' },
+  { id: 4, port: 'Dakar', country: 'Senegal', currency: 'Dollar' },
+  { id: 5, port: 'Walvis Bay', country: 'Namibia', currency: 'Dollar' },
+  { id: 6, port: 'Soyo', country: 'Angola', currency: 'Dollar' },
+  { id: 7, port: 'Luanda', country: 'Angola', currency: 'Dollar' },
+  { id: 8, port: 'Beira', country: 'Mozambique', currency: 'Dollar' },
+  { id: 9, port: 'Tampico', country: 'Mexico', currency: 'Dollar' },
+  { id: 10, port: 'Vitoria', country: 'Brazil', currency: 'Dollar' },
+  { id: 11, port: 'Rio', country: 'Brazil', currency: 'Dollar' },
+  { id: 12, port: 'Galvestone', country: 'USA', currency: 'Dollar' },
+  { id: 13, port: 'Fourchon', country: 'USA', currency: 'Dollar' },
+  { id: 14, port: 'Kuching', country: 'Malaysia', currency: 'Dollar' },
+  { id: 15, port: 'Bintulu', country: 'Malaysia', currency: 'Dollar' },
+  { id: 16, port: 'Dampier', country: 'Australia', currency: 'Dollar' },
+  { id: 17, port: 'Melbourne', country: 'Australia', currency: 'Dollar' },
+  { id: 18, port: 'Stavanger', country: 'Norway', currency: 'Dollar' },
+  { id: 19, port: 'Bergen', country: 'Norway', currency: 'Dollar' },
+  { id: 20, port: 'Dunkerque', country: 'France', currency: 'Dollar' },
+  { id: 21, port: 'Amsterdam', country: 'Netherlands', currency: 'Dollar' },
+  { id: 22, port: 'Ijmuiden', country: 'Netherlands', currency: 'Dollar' },
+  { id: 23, port: 'Den Helder', country: 'Netherlands', currency: 'Dollar' },
+  { id: 24, port: 'Singapore', country: 'Singapore', currency: 'Dollar' }
+]
+export const estimateTest = [
+  { id: 1, port: 'Fakedata', country: 'FAKE', currency: 'Dollar' }
+]
+export const unitCostValidate = [
+  { id: 1, name: 'UnitCost1', value: 100, tax: 0, fee: 40, unit: 'DAY', unitCost: 'DAY', description: `it's an unitCost it's test for lenght`, categorie: 'Categorie1' },
+  { id: 2, name: 'UnitCost2', value: 200, tax: 10, fee: 30, unit: 'DAY', unitCost: 'DAY', description: `it's an unitCost`, categorie: 'Categorie1' },
+  { id: 15, name: 'UnitCost3', value: 300, tax: 20, fee: 20, unit: 'DAY', unitCost: 'DAY', description: `it's an unitCost`, categorie: 'Categorie2' },
+  { id: 4, name: 'UnitCost4', value: 400, tax: 30, fee: 10, unit: 'DAY', unitCost: 'DAY', description: `it's an unitCost`, categorie: 'Categorie2' },
+  { id: 5, name: 'UnitCost5', value: 500, tax: 40, fee: 0, unit: 'DAY', unitCost: 'DAY', description: `it's an unitCost`, categorie: 'Categorie1' }
+]
+
+export const unitCostAwaiting = [
+  { id: 6, name: 'UnitCost6', value: 100, tax: 0, fee: 40, unit: 'DAY', unitCost: 'DAY', description: `it's an unitCost it's test for lenght`, categorie: 'Categorie2' },
+
 ]
