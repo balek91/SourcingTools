7b3e14601b8ce1142796504e69b728257596e27b
afaller <afaller@MSYDLAP0786.int.cggveritas.com>
2019-03-04 17:33:51 +0100

    add unitCost Managment in user Page

--- src/containers/userPage.js
+++ src/containers/userPage.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import Select from '../components/select'
 import Layout from './layout'
-import { countryOptions, serviceOptions, estimate } from '../database'
+import { countryOptions, serviceOptions, estimate, unitCostValidate, unitCostAwaiting, estimateTest } from '../database'
 import { TextMenu } from '../components/texts'
 import styled from 'styled-components'
 import DataTable from '../components/dataTableEstimate'
-
+import UnitCostManagment from '../components/unitCostManagment'
 
 
 const SearchDiv = styled.div`
@@ -54,19 +54,24 @@ export default class UserPage extends Component {
   state = {
     selectedLocation: null,
     selectService: null,
-    dataEstimateTable : null,
-    showUnitCost : false
+    dataEstimateTable: [],
+    showUnitCost: false,
+    dataUnitCostValidate: [],
+    dataUnitCostAwaiting: [],
+    refreshRender: true
+
   }
   handleChangeLocation = (selectedLocation) => {
-    this.setState({ 
-      selectedLocation : selectedLocation,
-      dataEstimateTable : estimate})
+    this.setState({
+      selectedLocation: selectedLocation,
+      dataEstimateTable: estimate
+    })
     console.log(selectedLocation.value)
   }
   handleChangeService = (selectService) => {
-    this.setState({ 
-      selectService : selectService,
-      dataEstimateTable : estimate
+    this.setState({
+      selectService: selectService,
+      dataEstimateTable: estimateTest
     })
     console.log(selectService.value)
   }
@@ -74,16 +79,77 @@ export default class UserPage extends Component {
     console.log('page charger')
   }
   onEstimate = (id) => {
-    this.setState({showUnitCost : true})
+    this.setState({
+      showUnitCost: true,
+      dataUnitCostValidate: unitCostValidate,
+      dataUnitCostAwaiting: unitCostAwaiting
+    })
     console.log('ok', id)
   }
 
+  findRowNumber = (table, id) => {
+    let i = 0
+    let find = false
+    let value = -1
+    while (i < table.length && !find) {
+      if (table[i].id === id) {
+        value = i
+      }
+      i++
+    }
+    return value
+  }
+
+
+
+  removeUnitCost = (id) => {
+    console.log('remove Unit Cost', id)
+    let index = this.findRowNumber(this.state.dataUnitCostValidate, id)
+    let tableForRemove = this.state.dataUnitCostValidate
+    let tableForAdd = this.state.dataUnitCostAwaiting
+    let rowToMove
+    if (index > -1) {
+      rowToMove = tableForRemove[index]
+      tableForAdd.push(rowToMove)
+      tableForRemove.splice(index, 1)
+      this.setState({
+        dataUnitCostValidate: tableForRemove,
+        dataUnitCostAwaiting: tableForAdd
+      })
+    }
+  }
+
+  addUnitCost = (id) => {
+    let index = this.findRowNumber(this.state.dataUnitCostAwaiting, id)
+    let tableForRemove = this.state.dataUnitCostAwaiting
+    let tableForAdd = this.state.dataUnitCostValidate
+    let rowToMove
+    if (index > -1) {
+      rowToMove = tableForRemove[index]
+      tableForAdd.push(rowToMove)
+      tableForRemove.splice(index, 1)
+      this.setState({
+        dataUnitCostAwaiting: tableForRemove,
+        dataUnitCostValidate: tableForAdd
+      })
+    }
+
+    console.log('add Unit Cost', id)
+  }
+
   estimate = () => {
+    console.log('validate', this.state.dataUnitCostValidate)
+    console.log('awaiting', this.state.dataUnitCostAwaiting)
     console.log('estimate')
+    this.setState({
+      refreshRender: true
+
+    })
+
   }
 
   render() {
-    const { selectedLocation, selectService, dataEstimateTable, showUnitCost } = this.state
+    const { selectedLocation, selectService, dataEstimateTable, showUnitCost, dataUnitCostValidate, dataUnitCostAwaiting } = this.state
     return (
       <Layout>
         <SearchDiv>
@@ -104,21 +170,14 @@ export default class UserPage extends Component {
         {
           showUnitCost ? (
             <div>
-              <EstimateDiv>
-                <TextMenuCustom>List Unit Cost</TextMenuCustom>
-                <DataTable data={dataEstimateTable} funct={this.onEstimate} />
-              </EstimateDiv>
-                <EstimateDiv>
-                  <TextMenuCustom>Other Unit Cost</TextMenuCustom>
-                  <DataTable data={dataEstimateTable} funct={this.onEstimate} />
-                </EstimateDiv>
-                <DivEstimateButton>
-                  <EstimateButton type='button' onClick={this.estimate}>
-                    Estimate
+              <UnitCostManagment dataUnitCostValidate={dataUnitCostValidate} onRemove={this.removeUnitCost} dataUnitCostAwaiting={dataUnitCostAwaiting} OnAdd={this.addUnitCost} titleFirst={'List Unit Cost'} titleSecond={'Other Unit Cost'} />
+              <DivEstimateButton>
+                <EstimateButton type='button' onClick={this.estimate}>
+                  Estimate
                   </EstimateButton>
-                </DivEstimateButton>
+              </DivEstimateButton>
             </div>
-          ):(null)
+          ) : (null)
         }
       </Layout>
     )
