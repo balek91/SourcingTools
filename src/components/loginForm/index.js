627ab641aeae0cbf1a89bec268609ff9447045c0
afaller <afaller@MSYDLAP0786.int.cggveritas.com>
2019-03-04 17:33:25 +0100

    change route after login button press ( change /home by /userPage)

--- src/components/loginForm/index.js
+++ src/components/loginForm/index.js
@@ -66,7 +66,7 @@ class LoginForm extends React.Component {
     }
     // mettre axios ici
     console.log(this.state.email, this.state.password)
-    history.push('/home')
+    history.push('/userPage')
   }
 
   render() {
