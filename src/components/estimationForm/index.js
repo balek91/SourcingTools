09f01f6629813b352028bc041b824d30d0512a12
afaller <afaller@MSYDLAP0786.int.cggveritas.com>
2019-03-27 17:29:27 +0100

    update location form

--- src/components/estimationForm/index.js
+++ src/components/estimationForm/index.js
@@ -1,22 +1,35 @@
 
 import React from 'react'
 import styled from 'styled-components'
-import { CommonText } from '../texts'
+import { TextMenu, CommonText } from '../texts'
 import Input from '../input'
 import Select from '../select'
-import { locationOptions, currencyOptions, serviceOptions } from '../../database'
+import { locationOptions, currencyOptions, serviceOptions, unitOptions, unitCostOptions, categorieOptions } from '../../database'
+import DataTable from '../unitCostNewEstimation'
 
 const Form = styled.form`
-  display: flex;
-  margin-top : 10px;
-  flex-direction: row;
-  text-align: center;
+
 `
+
+const GlobalDiv = styled.div`
+display: flex;
+margin-top : 10px;
+text-align: center;
+flex-direction : column; `
+
+
+const RowDiv = styled.div`
+display : flex;
+flex-direction : row;
+justify-content : center;
+flex : 1;`
+
 const StyledInput = styled(Input)`
 height : 16px;
 width : 36vh;
 `
-
+const StyledCheckbox = styled.input`
+`
 const StyledSubmit = styled.button`
 width : 36vh;
 padding: 0px;
@@ -25,17 +38,37 @@ border-radius : 4px;
 font-size : 18px;
 `
 
-const RowDiv = styled.div`
+const StandardDiv = styled.div`
 display :flex;
 justify-content :center;
-flex : 1;`
+flex : 1;
+align-items : center;
+`
+
+const TextMenuCustom = styled(TextMenu)`
+font-size : 25px;
+`
 
 export default class App extends React.Component {
   state = {
     location: '',
     currency: '',
     service: '',
-    errorMessage: ''
+    errorMessage: '',
+    name: '',
+    defaultValue: '',
+    unit: '',
+    unitCost: '',
+    description: '',
+    categorie: '',
+    defaultList: true,
+    dataUnitCost: [],
+    id: 0
+  }
+
+  onRemove = (rowId) => {
+    const newData = this.state.dataUnitCost.filter(x => x.id !== rowId);
+    this.setState({ dataUnitCost: newData });
   }
 
   inputHandler = event => {
@@ -43,8 +76,45 @@ export default class App extends React.Component {
       console.log(this.state)
     })
   }
-  clearErrorMessages = () => {
-    this.setState({ errorMessage: '' })
+
+  inputHandlerNumber = event => {
+    const re = /[-+]?([0-9]*\.[0-9]+|[0-9]+)/
+    if (event.target.value === '' || re.test(event.target.value)) {
+      this.setState({ [event.target.name]: event.target.value }, () => {
+        console.log(this.state)
+      })
+    }
+  }
+  clearFields = () => {
+    this.setState({
+      location: '',
+      currency: '',
+      service: '',
+      errorMessage: '',
+      name: '',
+      defaultValue: '',
+      unit: '',
+      unitCost: '',
+      description: '',
+      categorie: '',
+      defaultList: true,
+      dataUnitCost: [],
+      id: 0
+    })
+  }
+
+  clearFieldsUnitCost = () => {
+    this.setState({
+      errorMessage: '',
+      name: '',
+      defaultValue: '',
+      unit: '',
+      unitCost: '',
+      description: '',
+      categorie: '',
+      defaultList: true,
+      id: this.state.id + 1
+    })
   }
   checkInputs = () => {
     if (this.state.country === '' || this.state.port === '' || this.state.currency === '') {
@@ -52,6 +122,16 @@ export default class App extends React.Component {
         errorMessage: 'Veuillez remplir tous les champs'
       })
       alert('Veuillez remplir tous les champs')
+      return false
+    }
+    return true
+  }
+  checkInputsUnitCost = () => {
+    if (this.state.name === '' || this.state.defaultValue === '' || this.state.unit === '') {
+      this.setState({
+        errorMessage: 'Veuillez remplir tous les champs du Unit Cost'
+      })
+      alert('Veuillez remplir tous les champs du Unit Cost')
       return false
     }
     return true
@@ -72,33 +152,129 @@ export default class App extends React.Component {
     console.log(selectedService.value)
   }
 
+  handleChangeUnit = (selectedUnit) => {
+    this.setState({ unit: selectedUnit })
+    console.log(selectedUnit.value)
+  }
+
+  handleChangeUnitCost = (selectedUnitCost) => {
+    this.setState({ unitCost: selectedUnitCost })
+    console.log(selectedUnitCost.value)
+  }
+
+  handleChangeCategorie = (selectedCategorie) => {
+    this.setState({ categorie: selectedCategorie })
+    console.log(selectedCategorie.value)
+  }
+
   handleSubmit = event => {
     event.preventDefault()
-    this.clearErrorMessages()
     if (!this.checkInputs()) {
       console.log('err')
       return
     }
-    console.log(this.state.location.id, this.state.currency.id, this.state.service.id)
+    console.log(this.state.location.id, this.state.currency.id, this.state.service.id, this.state.dataUnitCost)
+    this.clearFields()
   }
 
+  submitNewUnitCost = event => {
+    event.preventDefault()
+
+    if (!this.checkInputsUnitCost()) {
+      console.log('err')
+      return
+    }
+    console.log(this.state.name, this.state.defaultValue, this.state.unit.value, this.state.unitCost.value, this.state.description, this.state.categorie.value, this.state.defaultList)
+    let item = {
+      id: this.state.id,
+      name: this.state.name,
+      defaultValue: this.state.defaultValue,
+      unit: this.state.unit,
+      unitCost: this.state.unitCost,
+      description: this.state.description,
+      categorie: this.state.categorie,
+      defaultList: this.state.defaultList
+    }
+    const newdataUnitCostAwaiting = [...this.state.dataUnitCost, item]
+    this.setState({
+      dataUnitCost: newdataUnitCostAwaiting
+    })
+    this.clearFieldsUnitCost()
+
+
+  }
+
+  handleChangeCheckBox = () => {
+    this.setState({
+      defaultList: !this.state.defaultList
+    })
+  }
+
+
+
   render() {
-    const { location, currency, service } = this.state
+    const { location, currency, service, unit, unitCost, categorie, defaultList, description, defaultValue, name, dataUnitCost } = this.state
     return (
-      <Form onSubmit={this.handleSubmit}>
+      <GlobalDiv>
+        <Form onSubmit={this.handleSubmit} id='estimation'></Form>
         <RowDiv>
-          <Select data={locationOptions} placeholder={'Location'} value={location} handleChange={this.handleChangeLocation} />
+          <StandardDiv>
+            <Select data={locationOptions} placeholder={'Location'} value={location} handleChange={this.handleChangeLocation} />
+          </StandardDiv>
+          <StandardDiv>
+            <Select data={serviceOptions} placeholder={'Service'} value={service} handleChange={this.handleChangeService} />
+          </StandardDiv>
+          <StandardDiv>
+            <Select data={currencyOptions} placeholder={'Currency'} value={currency} handleChange={this.handleChangeCurrency} />
+          </StandardDiv>
+        </RowDiv>
+        <GlobalDiv>
+          <RowDiv>
+            <TextMenuCustom>Unit Cost</TextMenuCustom>
+          </RowDiv>
+
+          <Form id='unitCost' onSubmit={this.submitNewUnitCost}></Form>
+          <RowDiv>
+            <StandardDiv>
+              <StyledInput onChange={this.inputHandler} placeholder='Name' name='name' value={name} />
+            </StandardDiv>
+            <StandardDiv>
+              <StyledInput onChange={this.inputHandlerNumber} placeholder='Default Value' name='defaultValue' value={defaultValue} />
+            </StandardDiv>
+            <StandardDiv>
+              <Select data={unitOptions} placeholder={'Unit'} value={unit} handleChange={this.handleChangeUnit} />
+            </StandardDiv>
+            <StandardDiv>
+              <Select data={unitCostOptions} placeholder={'Unit Cost'} value={unitCost} handleChange={this.handleChangeUnitCost} />
+            </StandardDiv>
+          </RowDiv>
+          <RowDiv>
+
+            <StandardDiv>
+              <StyledInput onChange={this.inputHandler} placeholder='Description' name='description' value={description} />
+            </StandardDiv>
+            <StandardDiv>
+              <Select data={categorieOptions} placeholder={'Categorie'} value={categorie} handleChange={this.handleChangeCategorie} />
+            </StandardDiv>
+            <StandardDiv>
+              <CommonText>Default :</CommonText>
+              <StyledCheckbox type='checkbox' defaultChecked={defaultList} onChange={this.handleChangeCheckBox} />
+            </StandardDiv>
+            <StandardDiv>
+              <StyledSubmit type='submit' form='unitCost' >Add</StyledSubmit>
+            </StandardDiv>
+          </RowDiv>
+        </GlobalDiv>
+
+
+        <RowDiv>
+          <DataTable dataUnitCost={dataUnitCost} funct={this.onRemove}></DataTable>
         </RowDiv>
         <RowDiv>
-          <Select data={serviceOptions} placeholder={'Service'} value={service} handleChange={this.handleChangeService} />
+          <StyledSubmit type='submit' form='estimation' >Create</StyledSubmit>
         </RowDiv>
-        <RowDiv>
-          <Select data={currencyOptions} placeholder={'Currency'} value={currency} handleChange={this.handleChangeCurrency} />
-        </RowDiv>
-        <RowDiv>
-          <StyledSubmit type='submit' >Create</StyledSubmit>
-        </RowDiv>
-      </Form>
+      </GlobalDiv>
+
     )
   }
 }
