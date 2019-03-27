0a7b3c38043a12a991d1527b288819accd7f2841
afaller <afaller@MSYDLAP0786.int.cggveritas.com>
2019-03-27 17:31:07 +0100

    Replace service form by referential Form

--- src/components/referentialForm/index.js
+++ src/components/referentialForm/index.js
@@ -1,10 +1,9 @@
 
 import React from 'react'
 import styled from 'styled-components'
-import { CommonText } from '../texts'
 import Input from '../input'
 import Select from '../select'
-import { countryOptions, currencyOptions } from '../../database'
+import { referentialTypeOptions } from '../../database'
 
 const Form = styled.form`
   display: flex;
@@ -14,11 +13,11 @@ const Form = styled.form`
 `
 const StyledInput = styled(Input)`
 height : 16px;
-width : 50vh;
+width : 36vh;
 `
 
 const StyledSubmit = styled.button`
-width : 50vh;
+width : 36vh;
 padding: 0px;
 height : 36px;
 border-radius : 4px;
@@ -32,7 +31,8 @@ flex : 1;`
 
 export default class App extends React.Component {
   state = {
-    service: '',
+    referential: '',
+    referentialType: '',
     errorMessage: ''
   }
 
@@ -44,8 +44,11 @@ export default class App extends React.Component {
   clearErrorMessages = () => {
     this.setState({ errorMessage: '' })
   }
+  clearFields = () => {
+    this.setState({ referential: '', referentialType: '' })
+  }
   checkInputs = () => {
-    if (this.state.service === '') {
+    if (this.state.referential === '' || this.state.referentialType === '') {
       this.setState({
         errorMessage: 'Veuillez remplir tous les champs '
       })
@@ -61,22 +64,30 @@ export default class App extends React.Component {
       console.log('err')
       return
     }
-    console.log(this.state.service)
+    console.log(this.state.referentialType.value, this.state.referential)
+    this.clearFields()
+  }
+
+  handleChangeReferentialType = (selectedReferentialType) => {
+    this.setState({ referentialType: selectedReferentialType })
+    console.log(selectedReferentialType.value)
   }
 
   render() {
+    const { referentialType, referential } = this.state
     return (
       <Form onSubmit={this.handleSubmit}>
+
         <RowDiv>
+          <StyledInput onChange={this.inputHandler} placeholder='Referential' name='referential' value={referential} />
         </RowDiv>
         <RowDiv>
-          <StyledInput onChange={this.inputHandler} placeholder='Service' name='service' />
+          <Select data={referentialTypeOptions} placeholder={'Referential Type'} value={referentialType} handleChange={this.handleChangeReferentialType} />
         </RowDiv>
         <RowDiv>
           <StyledSubmit type='submit' >Create</StyledSubmit>
         </RowDiv>
-        <RowDiv>
-        </RowDiv>
+
       </Form>
     )
   }
