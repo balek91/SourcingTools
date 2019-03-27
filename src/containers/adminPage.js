b24d0d50cf5a05ea14b9fdaca4894fea4706df13
afaller <afaller@MSYDLAP0786.int.cggveritas.com>
2019-03-27 17:28:23 +0100

    Implementation of all components in admin Page

--- src/containers/adminPage.js
+++ src/containers/adminPage.js
@@ -4,14 +4,23 @@ import Button from '../components/buttonCustom'
 import Layout from './layout'
 import TitleComp from '../components/titleComponent'
 import LocationForm from '../components/locationForm'
-import ServiceForm from '../components/serviceForm'
+import ReferentialForm from '../components/referentialForm'
 import EstimationForm from '../components/estimationForm'
+import ManageLocation from '../components/manageLocation'
+import ManageEstimation from '../components/manageEstimation'
+import ManageReferential from '../components/manageReferential'
+import { locationOptions, estimate, referentialOptions } from '../database'
 
 const StandardDiv = styled.div`
 display: flex;
 flex-direction : column;
 margin-top : 10px;
 `
+
+const CenterDiv = styled(StandardDiv)`
+align-items : center;
+`
+
 const RowDiv = styled.div`
 display : flex;
 flex-direction : row;
@@ -26,17 +35,25 @@ justify-content : center;
 
 export default class AdminPage extends Component {
 
-  componentDidMount() {
-  }
-
   state = {
     creationShow: true,
     estimationShow: false,
     locationShow: false,
-    serviceShow: false,
+    referentialShow: false,
     createLocationShow: false,
     createEstimationShow: false,
-    createServiceShow: false
+    createReferentialShow: false,
+    dataLocation: [],
+    dataEstimation: [],
+    dataReferential: []
+  }
+
+  componentDidMount() {
+    this.setState({
+      dataLocation: locationOptions,
+      dataEstimation: estimate,
+      dataReferential: referentialOptions
+    })
   }
 
   changeCreationStatus = () => {
@@ -54,9 +71,9 @@ export default class AdminPage extends Component {
       locationShow: !this.state.locationShow
     })
   }
-  changeServiceStatus = () => {
+  changeReferentialStatus = () => {
     this.setState({
-      serviceShow: !this.state.serviceShow
+      referentialShow: !this.state.referentialShow
     })
   }
 
@@ -64,26 +81,46 @@ export default class AdminPage extends Component {
     this.setState({
       createLocationShow: true,
       createEstimationShow: false,
-      createServiceShow: false
+      createReferentialShow: false
     })
   }
   createNewEstimation = () => {
     this.setState({
       createLocationShow: false,
       createEstimationShow: true,
-      createServiceShow: false
+      createReferentialShow: false
     })
   }
-  createNewService = () => {
+  createNewReferential = () => {
     this.setState({
       createLocationShow: false,
       createEstimationShow: false,
-      createServiceShow: true
+      createReferentialShow: true
     })
   }
 
+  onRemove = (rowId, type) => {
+    if (type === 'location') {
+      const newData = this.state.dataLocation.filter(x => x.id !== rowId)
+      this.setState({ dataLocation: newData })
+      console.log('remove location ', rowId)
+    }
+    else if (type === 'estimation') {
+      const newData = this.state.dataEstimation.filter(x => x.id !== rowId)
+      this.setState({ dataEstimation: newData })
+      console.log('remove estimation ', rowId)
+    }
+    else if (type === 'referential') {
+      const newData = this.state.dataReferential.filter(x => x.id !== rowId)
+      this.setState({ dataReferential: newData })
+      console.log('remove referential ', rowId)
+    } else {
+      console.log('type unknown')
+    }
+  }
+
   render() {
-    const { creationShow, estimationShow, locationShow, serviceShow, createLocationShow, createEstimationShow, createServiceShow } = this.state
+    const { creationShow, estimationShow, locationShow, referentialShow, createLocationShow, createEstimationShow, createReferentialShow, dataLocation, dataEstimation, dataReferential } = this.state
     return (
       <Layout>
         < StandardDiv>
@@ -95,7 +132,7 @@ export default class AdminPage extends Component {
                   <Button func={this.createNewLocation} text={'Create Location'}></Button>
                 </ButtonDiv>
                 <ButtonDiv>
-                  <Button func={this.createNewService} text={'Create Service'}></Button>
+                  <Button func={this.createNewReferential} text={'Create Referential'}></Button>
                 </ButtonDiv>
                 <ButtonDiv>
                   <Button func={this.createNewEstimation} text={'Create Estimate'}></Button>
@@ -107,8 +144,8 @@ export default class AdminPage extends Component {
               {createEstimationShow ?
                 <EstimationForm />
                 : null}
-              {createServiceShow ?
-                <ServiceForm />
+              {createReferentialShow ?
+                <ReferentialForm />
                 : null}
             </StandardDiv>
             : (null)
@@ -117,19 +154,25 @@ export default class AdminPage extends Component {
         < StandardDiv>
           <TitleComp func={this.changeEstimationStatus} bool={estimationShow} text={'Estimation View/Update'} />
           {estimationShow ?
-            <p>Creation</p>
+            < CenterDiv>
+              <ManageEstimation func={this.onRemove} data={dataEstimation}></ManageEstimation>
+            </CenterDiv>
             : (null)}
         </StandardDiv>
         < StandardDiv>
           <TitleComp func={this.changeLocationStatus} bool={locationShow} text={'Location View/Update'} />
           {locationShow ?
-            <p>Creation</p>
+            < CenterDiv>
+              <ManageLocation func={this.onRemove} data={dataLocation}></ManageLocation>
+            </CenterDiv>
             : (null)}
         </StandardDiv>
         < StandardDiv>
-          <TitleComp func={this.changeServiceStatus} bool={serviceShow} text={'Service View/Update'} />
-          {serviceShow ?
-            <p>Creation</p>
+          <TitleComp func={this.changeReferentialStatus} bool={referentialShow} text={'Referential View/Update'} />
+          {referentialShow ?
+            < CenterDiv>
+              <ManageReferential func={this.onRemove} data={dataReferential}></ManageReferential>
+            </CenterDiv>
             : (null)}
         </StandardDiv>
       </Layout >
